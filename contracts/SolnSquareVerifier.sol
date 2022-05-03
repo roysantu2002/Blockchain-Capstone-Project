1f2535cbe826ea59e1c2df5fd6a80bf8bead07e1
“Santu <roysantu2002@yahoo.com>
2022-05-03 18:39:24 +0530

    Fixed and added all the feedback

--- contracts/SolnSquareVerifier.sol
+++ contracts/SolnSquareVerifier.sol
@@ -1,77 +1,95 @@
 pragma solidity 0.5.11;
 
-import {ERC721Mintable} from "./ERC721MintableComplete.sol";
+import {ERC721MintableComplete} from "./ERC721MintableComplete.sol";
 
-// TODO define another contract named SolnSquareVerifier that inherits from your ERC721Mintable class
-contract SolnSquareVerifier is ERC721Mintable {
+// TODO define another contract named SolnSquareVerifier that inherits from your ERC721MintableComplete class
+contract SolnSquareVerifier is ERC721MintableComplete {
     // TODO define a contract call to the zokrates generated solidity contract <Verifier> or <renamedVerifier>
-    SquareVerifier verifierContract;
+    Verifier verifierContract;
 
     constructor(address verifierAddress) public {
-        verifierContract = SquareVerifier(verifierAddress);
+        verifierContract = Verifier(verifierAddress);
     }
 
     // TODO define a solutions struct that can hold an index & an address
     struct Solution {
         address toAddress;
         uint256 tokenIndex;
+        bool solutionExists;
+        bool minted;
     }
 
     // TODO define an array of the above struct
-    Solution[] submittedSolutions;
+
+    uint256 public totalSolutions = 0;
 
     // TODO define a mapping to store unique solutions submitted
     mapping(bytes32 => Solution) oneSolutions;
 
     // TODO Create an event to emit when a solution is added
-    event SolutionAdded(
-        address indexed toAddress,
-        uint256 indexed tokenIndex,
-        bytes32 indexed key
-    );
+    event SolutionAdded(uint256 tokenIndex, address indexed toAddress);
 
     // TODO Create a function to add the solutions to the array and emit the event
-    function addupSolutions(
-        address toAddress,
-        uint256 tokenIndex,
-        bytes32 key
-    ) internal {
-        Solution memory soln = Solution({tokenId: tokenIndex, to: toAddress});
-        submittedSolutions.push(soln);
-        oneSolutions[key] = soln;
-        emit SolutionAdded(toAddress, tokenIndex, key);
+
+    function addSolution(
+        uint256[2] memory a,
+        uint256[2][2] memory b,
+        uint256[2] memory c,
+        uint256[2] memory input
+    ) public {
+        bytes32 solutionHash = keccak256(abi.encodePacked(input[0], input[1]));
+        require(
+            oneSolutions[solutionHash].solutionExists == false,
+            "Solution exists"
+        );
+
+        bool verified = verifierContract.verifyTx(a, b, c, input);
+        require(verified, "Solution could not be verified");
+
+        oneSolutions[solutionHash] = Solution(
+            msg.sender,
+            totalSolutions,
+            true,
+            false
+        );
+
+        emit SolutionAdded(totalSolutions, msg.sender);
+        totalSolutions += 1;
     }
 
     // TODO Create a function to mint new NFT only after the solution has been verified
+    //  - make sure the solution is unique (has not been used before)
+    //  - make sure you handle metadata as well as tokenSuplly
+    function mintNewNFT(
+        uint256 a,
+        uint256 b,
+        address to
+    ) public {
+        bytes32 solutionHash = keccak256(abi.encodePacked(a, b));
 
-    function mintUREMTokenoken(
-        address toAddress,
-        uint256 tokenIndex,
-        uint256[2] memory _one,
-        uint256[2][2] memory _two,
-        uint256[2] memory _out,
-        uint256[2] memory input
-    ) public whenNotPaused {
-        bytes32 _outKey = keccak256(abi.encodePacked(_one, _two, _out, input));
         require(
-            oneSolutions[_outKey].to == address(0),
-            "Solution is already used"
+            oneSolutions[solutionHash].solutionExists == true,
+            "Solution does not exist"
         );
         require(
-            verifierContract.verifyTx(_one, _two, _out, input),
-            "Solution is incorrect"
+            oneSolutions[solutionHash].minted == false,
+            "Token already minted for this solution"
         );
-        addupSolutions(toAddress, tokenIndex, _outKey);
-        super.mint(toAddress, tokenIndex);
+        require(
+            oneSolutions[solutionHash].toAddress == msg.sender,
+            "Owner can use it to mint a token"
+        );
+        super.mint(to, oneSolutions[solutionHash].tokenIndex);
+        oneSolutions[solutionHash].minted = true;
     }
 }
 
 // TODO define a contract call to the zokrates generated solidity contract <Verifier> or <renamedVerifier>
 contract Verifier {
     function verifyTx(
-        uint256[2] memory _one,
-        uint256[2][2] memory _two,
-        uint256[2] memory _out,
+        uint256[2] memory a,
+        uint256[2][2] memory b,
+        uint256[2] memory c,
         uint256[2] memory input
     ) public returns (bool r);
 }
